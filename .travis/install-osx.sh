cfc6729a8c8f9c20b662016502482d1026e1abc8
Norge Fajardo Vega <fajardovega@gmail.com>
2018-07-06 19:24:35 -0400

    [ci] refs #16 - Force build

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -10,8 +10,9 @@ brew install swig;
 brew install gimme;
 
 # Install Python
-pyenv install $PYTHON
-pyenv local 2.7
+pyenv install 2.7.0
+pyenv install 3.4.8
+pyenv local 2.7 3.4
 
 # Prepare and initialize pyenv environment
 export PYENV_VERSION=$PYTHON

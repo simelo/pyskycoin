5add7b8a0efb80a011cc345e3f62444c9607c2b4
Marcos Maceo <marcosmaceowork@gmail.com>
2019-09-17 11:57:09 -0400

    [ci][docker] Reset build_wheels.sh to correct definition

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -19,13 +19,12 @@ source /etc/profile
 go version
 go env
 
-ls -la /io/lib/skyapi
+ls -la
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install -r /io/requirements.dev.txt
-  "${PYBIN}/pip" install -r /io/lib/skyapi/requirements.txt
   "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-  "${PYBIN}/pip" wheel /io/lib/skyapi -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
